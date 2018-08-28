258931bd5adfb2e9602bab5376d912203c0d4098
Oskar Sharipov <oskar.sharipov@tuta.io>
2018-08-28 10:10:02 +0300

    Add example of the bot

---
+++ test_bot.py
@@ -0,0 +1,16 @@
+import telebot
+import logging
+import sending_queue
+
+token = "<TOKEN>"
+bot = telebot.TeleBot(token)
+queue = sending_queue.SendingQueue(bot, logging)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message):
+    queue.add_text_message(message.chat.id, 'Hi')
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
